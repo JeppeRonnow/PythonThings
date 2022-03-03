5e2be48556f0325524ac6ca08ccb356953305b02
DNIIBOY <daniel.nettelfield@gmail.com>
2022-03-03 21:35:27 +0100

    mine øjne gør ikke lige så ondt nu

--- Valorant_instalock/InstalockV1.1.py
+++ Valorant_instalock/InstalockV1.1.py
@@ -3,7 +3,12 @@ import pyscreenshot as imggrab
 from time import sleep
 
 
-def agentSel():
+def agent_select():
+    global agent_X
+    global agent_Y
+    agent_X = 626
+    agent_Y = 925
+
     slotInput = input("Select agent slot number: ")
     if slotInput.strip().isdigit():
         slotInput = int(slotInput)
@@ -11,29 +16,20 @@ def agentSel():
         print("Not a number")
         quit()
 
-    while 1 < slotInput > 18:
+    while slotInput < 1 or slotInput > 18:
         print("Ivalid number, has to be between 1-18")
         slotInput = int(input("Select agent slot number: "))
 
-    global agent_X
-    agent_X = 626
 
-    global agent_Y
-    agent_Y = 925
-
-    if slotInput == 1:
-        agent_X = agent_X
-    elif slotInput > 1 and slotInput < 10:
+    if 1 < slotInput < 10:
         agent_X = agent_X+84*(slotInput-1)
-    elif slotInput > 9 and slotInput < 19:
+    elif slotInput > 9:
         agent_X = agent_X+84*(slotInput-10)
         agent_Y = agent_Y + 84
 
-def load_status():
-    global load
-    load = True
 
-    while load == 1:
+def wait_for_load():
+    while True:
         img = imggrab.grab(bbox=(841, 784, 841+1, 784+1))
         img2 = imggrab.grab(bbox=(950, 866, 950+1, 866+1))
 
@@ -41,19 +37,9 @@ def load_status():
         rgb_pixel_value2 = img2.getpixel((0, 0))
 
         if rgb_pixel_value == (255,255,255) and rgb_pixel_value2 == (234,238,178):
-            load = False
-            return True
+            return
         
 
-def main():
-    agentSel()
-
-    if load_status() == True:
-        lockin()
-        print("agent locked")
-        sleep(2)
-        quit()
-        
 def lockin():
     auto.click(agent_X, agent_Y)
     auto.click(clicks=1)
@@ -61,5 +47,17 @@ def lockin():
     auto.moveTo(1085,820)
     auto.drag(-15,0,duration=0.02)
     auto.click(clicks=2)
+        
 
-main()
+def main():
+    agent_select()
+
+    wait_for_load()
+    lockin()
+    print("agent locked")
+    sleep(2)
+    quit()
+
+
+if __name__ == "__main__":
+    main()
